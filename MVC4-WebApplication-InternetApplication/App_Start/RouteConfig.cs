f709eb8e1f2dc2b35e0448c2e7d88e6a9496ca3c
David Siew <davidsiew@outlook.com>
2013-09-16 13:51:31 +0100

    - Add various routes - Add BlogController.List

--- MVC4-WebApplication-InternetApplication/App_Start/RouteConfig.cs
+++ MVC4-WebApplication-InternetApplication/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace MVC4_WebApplication_InternetApplication
@@ -13,10 +9,26 @@ namespace MVC4_WebApplication_InternetApplication
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("BlogArchive",
+                "{year}/{month}/{day}",
+                new { controller = "Blog", action = "List", month = "1", day = "1" },
+                new { year = @"\d{2}|\d{4}", month = @"\d{1,2}", day = @"\d{1,2}" }
+                );
+
+            routes.MapRoute("Post",
+                "{title}",
+                new { controller = "Blog", action = "Post" }
+                );
+
+            routes.MapRoute("Tags",
+                "tags/{tag}",
+                new { controller = "Blog", action = "Tags" }
+            );
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ MVC4-WebApplication-InternetApplication/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC4_WebApplication_InternetApplication.Controllers
+{
+    public class BlogController : Controller
+    {
+        //
+        // GET: /Blog/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List(int year, int month, int day)
+        {
+            return Content(string.Format("You've just hit BlogController.List with year: {0}, month: {1} and day: {2}", year, month, day));
+        }
+
+    }
+}
